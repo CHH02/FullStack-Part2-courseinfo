0575313ecc81ab0c8bbe04b380a308c47f1df4ec
CHH02 <clayhop901@gmail.com>
2026-02-12 12:17:37 -0600

    Fixing misconfiguration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chh02.github.io/FullStack-anecdotes/',
+  base: 'https://chh02.github.io/FullStack-Par2-courseinfo/',
 })
